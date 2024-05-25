ca3ce406d11a2f38097db35e0b487bb17ae16792
Paras Raut <parasraut2511@gmail.com>
2024-05-26 00:20:21 +0530

    error fixed

--- components/Posts/Post.js
+++ components/Posts/Post.js
@@ -1,81 +1,171 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import React, { useRef } from 'react';
 import styled from '@emotion/styled';
-import Post from './Post';
-import Container from '../common/Container';
 
-import { useWindowWidth } from '../ContextApi/useWindowWidth';
+const PostContainer = styled.div(() => ({
+  width: '300px',
+  margin: '10px',
+  border: '1px solid #ccc',
+  borderRadius: '5px',
+  overflow: 'hidden',
+}));
 
-const PostListContainer = styled.div(() => ({
+const CarouselContainer = styled.div(() => ({
+  position: 'relative',
+}));
+
+const Carousel = styled.div(() => ({
   display: 'flex',
-  flexWrap: 'wrap',
-  justifyContent: 'center',
+  overflowX: 'scroll',
+  scrollbarWidth: 'none',
+  msOverflowStyle: 'none',
+  '&::-webkit-scrollbar': {
+    display: 'none',
+  },
+  position: 'relative',
 }));
 
-const LoadMoreButton = styled.button(() => ({
-  padding: '10px 20px',
-  backgroundColor: '#007bff',
-  color: '#fff',
+const CarouselItem = styled.div(() => ({
+  flex: '0 0 auto',
+  scrollSnapAlign: 'start',
+}));
+
+const Image = styled.img(() => ({
+  width: '280px',
+  height: 'auto',
+  maxHeight: '300px',
+  padding: '10px',
+}));
+
+const Content = styled.div(() => ({
+  padding: '10px',
+  '& > h2': {
+    marginBottom: '16px',
+  },
+}));
+
+const Button = styled.button(() => ({
+  position: 'absolute',
+  top: '50%',
+  transform: 'translateY(-50%)',
+  backgroundColor: 'rgba(255, 255, 255, 0.5)',
   border: 'none',
-  borderRadius: 5,
+  color: '#000',
+  fontSize: '24px',
   cursor: 'pointer',
-  fontSize: 16,
-  marginTop: 20,
-  transition: 'background-color 0.3s ease',
-  fontWeight: 600,
-
+  height: '50px',
+  width: '50px',
+  zIndex: '1',
+  opacity: '0.8',
+  transition: 'opacity 0.3s ease',
   '&:hover': {
-    backgroundColor: '#0056b3',
-  },
-  '&:disabled': {
-    backgroundColor: '#808080',
-    cursor: 'default',
+    opacity: '1',
   },
 }));
 
-export default function Posts() {
-  const [posts, setPosts] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [start, setStart] = useState(0);
-  const [hasMore, setHasMore] = useState(true); 
+const PrevButton = styled(Button)`
+  left: 0;
+`;
 
-  const isSmallerDevice = useWindowWidth();
-  const limit = isSmallerDevice ? 5 : 10;
+const NextButton = styled(Button)`
+  right: 0;
+`;
 
-  const fetchPost = async () => {
-    setIsLoading(true);
-    const { data: newPosts } = await axios.get('/api/v1/posts', {
-      params: { start, limit },
-    });
-    setPosts(posts => [...posts, ...newPosts]);
-    setStart(start => start + limit);
-    setHasMore(newPosts.length > 0); 
-    setIsLoading(false);
+const UserContainer = styled.div(() => ({
+  display: 'flex',
+  alignItems: 'center',
+  marginBottom: '10px',
+}));
+
+const ProfileImage = styled.div(() => ({
+  width: '40px',
+  height: '40px',
+  borderRadius: '50%',
+  backgroundColor: '#008000',
+  color: '#fff',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  marginRight: '15px',
+  marginLeft: '15px',
+  marginTop: '15px',
+  fontSize: '20px',
+  fontWeight: 'bold',
+  textTransform: 'uppercase',
+}));
+
+const UserName = styled.div(() => ({
+  fontSize: '16px',
+  fontWeight: 'bold',
+  marginTop: '15px',
+}));
+
+const UserEmail = styled.div(() => ({
+  fontSize: '12px',
+}));
+
+const Post = ({ post }) => {
+  const carouselRef = useRef(null);
+
+  const handleNextClick = () => {
+    if (carouselRef.current) {
+      carouselRef.current.scrollLeft += carouselRef.current.offsetWidth;
+    }
   };
-
-  useEffect(() => {
-    fetchPost(); 
-  }, []); 
-
-  const handleClick = () => {
-    fetchPost();
+  
+  const handlePrevClick = () => {
+    if (carouselRef.current) {
+      carouselRef.current.scrollLeft -= carouselRef.current.offsetWidth;
+    }
   };
+  
+
+  const name = post.user?.name || 'Unknown User';
+  const email = post.user?.email || 'unknown@example.com';
+
+  const nameParts = name.split(' ');
+  const initials = `${nameParts[0][0]}${nameParts[nameParts.length - 1][0]}`;
 
   return (
-    <Container>
-      <PostListContainer>
-        {posts.map(post => (
-          <Post post={post} />
-        ))}
-      </PostListContainer>
-
-      {hasMore && ( 
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-          <LoadMoreButton onClick={handleClick} disabled={isLoading}>
-            {!isLoading ? 'Load More' : 'Loading...'}
-          </LoadMoreButton>
+    <PostContainer>
+      <UserContainer>
+        <ProfileImage>{initials}</ProfileImage>
+        <div>
+          <UserName>{name}</UserName>
+          <UserEmail>{email}</UserEmail>
         </div>
-      )}
-    </Container>
+      </UserContainer>
+      <CarouselContainer>
+        <Carousel ref={carouselRef}>
+          {post.images.map((image, index) => (
+            <CarouselItem key={index}>
+              <Image src={image.url} alt={image.title} />
+            </CarouselItem>
+          ))}
+        </Carousel>
+        <PrevButton onClick={handlePrevClick}>&#10094;</PrevButton>
+        <NextButton onClick={handleNextClick}>&#10095;</NextButton>
+      </CarouselContainer>
+      <Content>
+        <h2>{post.title}</h2>
+        <p>{post.body}</p>
+      </Content>
+    </PostContainer>
   );
-}
+};
+
+Post.propTypes = {
+  post: PropTypes.shape({
+    user: PropTypes.shape({
+      name: PropTypes.string,
+      email: PropTypes.string,
+    }),
+    images: PropTypes.arrayOf(PropTypes.shape({
+      url: PropTypes.string,
+      title: PropTypes.string,
+      body: PropTypes.string,
+    })),
+  }),
+};
+
+export default Post;
--- components/Posts/index.js
+++ components/Posts/index.js
@@ -1,168 +1,81 @@
-import PropTypes from 'prop-types';
-import React, { useRef } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import styled from '@emotion/styled';
+import Post from './Post';
+import Container from '../common/Container';
 
-const PostContainer = styled.div(() => ({
-  width: '300px',
-  margin: '10px',
-  border: '1px solid #ccc',
-  borderRadius: '5px',
-  overflow: 'hidden',
-}));
+import { useWindowWidth } from '../ContextApi/useWindowWidth';
 
-const CarouselContainer = styled.div(() => ({
-  position: 'relative',
-}));
-
-const Carousel = styled.div(() => ({
+const PostListContainer = styled.div(() => ({
   display: 'flex',
-  overflowX: 'scroll',
-  scrollbarWidth: 'none',
-  msOverflowStyle: 'none',
-  '&::-webkit-scrollbar': {
-    display: 'none',
-  },
-  position: 'relative',
-}));
-
-const CarouselItem = styled.div(() => ({
-  flex: '0 0 auto',
-  scrollSnapAlign: 'start',
-}));
-
-const Image = styled.img(() => ({
-  width: '280px',
-  height: 'auto',
-  maxHeight: '300px',
-  padding: '10px',
-}));
-
-const Content = styled.div(() => ({
-  padding: '10px',
-  '& > h2': {
-    marginBottom: '16px',
-  },
-}));
-
-const Button = styled.button(() => ({
-  position: 'absolute',
-  top: '50%',
-  transform: 'translateY(-50%)',
-  backgroundColor: 'rgba(255, 255, 255, 0.5)',
-  border: 'none',
-  color: '#000',
-  fontSize: '20px',
-  cursor: 'pointer',
-  height: '50px',
-}));
-
-const PrevButton = styled(Button)`
-  left: 10px;
-`;
-
-const NextButton = styled(Button)`
-  right: 10px;
-`;
-
-const UserContainer = styled.div(() => ({
-  display: 'flex',
-  alignItems: 'center',
-  marginBottom: '10px',
-}));
-
-const ProfileImage = styled.div(() => ({
-  width: '40px', 
-  height: '40px', 
-  borderRadius: '50%',
-  backgroundColor: '#008000',
-  color: '#fff',
-  display: 'flex',
+  flexWrap: 'wrap',
   justifyContent: 'center',
-  alignItems: 'center',
-  marginRight: '15px',
-  marginLeft: '15px',
-  marginTop: '15px', 
-  fontSize: '20px', 
-  fontWeight: 'bold',
-  textTransform: 'uppercase',
 }));
 
-const UserName = styled.div(() => ({
-  fontSize: '16px',
-  fontWeight: 'bold',
-  marginTop: '15px', 
+const LoadMoreButton = styled.button(() => ({
+  padding: '10px 20px',
+  backgroundColor: '#007bff',
+  color: '#fff',
+  border: 'none',
+  borderRadius: 5,
+  cursor: 'pointer',
+  fontSize: 16,
+  marginTop: 20,
+  transition: 'background-color 0.3s ease',
+  fontWeight: 600,
+
+  '&:hover': {
+    backgroundColor: '#0056b3',
+  },
+  '&:disabled': {
+    backgroundColor: '#808080',
+    cursor: 'default',
+  },
 }));
 
-const UserEmail = styled.div(() => ({
-  fontSize: '12px',
-}));
-const Post = ({ post }) => {
-  const carouselRef = useRef(null);
+export default function Posts() {
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [start, setStart] = useState(0);
+  const [hasMore, setHasMore] = useState(true); 
 
-  const handleNextClick = () => {
-    if (carouselRef.current) {
-      carouselRef.current.scrollBy({
-        left: 300,
-        behavior: 'smooth',
-      });
-    }
+  const isSmallerDevice = useWindowWidth();
+  const limit = isSmallerDevice ? 5 : 10;
+
+  const fetchPost = async () => {
+    setIsLoading(true);
+    const { data: newPosts } = await axios.get('/api/v1/posts', {
+      params: { start, limit },
+    });
+    setPosts(posts => [...posts, ...newPosts]);
+    setStart(start => start + limit);
+    setHasMore(newPosts.length > 0); 
+    setIsLoading(false);
   };
 
-  const handlePrevClick = () => {
-    if (carouselRef.current) {
-      carouselRef.current.scrollBy({
-        left: -300,
-        behavior: 'smooth',
-      });
-    }
+  useEffect(() => {
+    fetchPost(); 
+  }, []); 
+
+  const handleClick = () => {
+    fetchPost();
   };
-
-  const name = "Paras Raut";
-  const email = "parasraut2511@gmail.com";
-
-  const nameParts = name.split(' ');
-  const initials = `${nameParts[0][0]}${nameParts[nameParts.length - 1][0]}`;
 
   return (
-    <PostContainer>
-      <UserContainer>
-        <ProfileImage>{initials}</ProfileImage>
-        <div>
-          <UserName>{name}</UserName>
-          <UserEmail>{email}</UserEmail>
+    <Container>
+      <PostListContainer>
+        {posts.map(post => (
+          <Post post={post} />
+        ))}
+      </PostListContainer>
+
+      {hasMore && ( 
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <LoadMoreButton onClick={handleClick} disabled={isLoading}>
+            {!isLoading ? 'Load More' : 'Loading...'}
+          </LoadMoreButton>
         </div>
-      </UserContainer>
-      <CarouselContainer>
-        <Carousel ref={carouselRef}>
-          {post.images.map((image, index) => (
-            <CarouselItem key={index}>
-              <Image src={image.url} alt={post.title} />
-            </CarouselItem>
-          ))}
-        </Carousel>
-        <PrevButton onClick={handlePrevClick}>&#10094;</PrevButton>
-        <NextButton onClick={handleNextClick}>&#10095;</NextButton>
-      </CarouselContainer>
-      <Content>
-        <h2>{post.title}</h2>
-        <p>{post.body}</p>
-      </Content>
-    </PostContainer>
+      )}
+    </Container>
   );
-};
-
-Post.propTypes = {
-  post: PropTypes.shape({
-    user: PropTypes.shape({
-      name: PropTypes.string,
-      email: PropTypes.string,
-    }),
-    images: PropTypes.arrayOf(PropTypes.shape({
-      url: PropTypes.string,
-    })),
-    title: PropTypes.string,
-    body: PropTypes.string,
-  }),
-};
-
-export default Post;
+}
